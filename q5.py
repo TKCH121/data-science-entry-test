2080b055c9234e6e66c8e92dce928d61c5113f48
TKCH121 <timothykoo@hotmail.com>
2026-06-23 13:47:32 +0800

    Update q5.py

--- q5.py
+++ q5.py
@@ -6,14 +6,14 @@
 """
 
 def check_divisibility(num, divisor):
-    if not (isinstance(num, (int, float)) and isinstance(divisor, (int, float))):
+    if not (isinstance(num, (int, float)) and isinstance(divisor, (int, float))): #check that both values is either an integer or a float, return -1 if not
         return -1
-    return num % divisor == 0
+    return num % divisor == 0 #modulo operator to return the remainder - remainder = 0 means the number is perfectly divisible without remainders
 
 
 # Task 2
 # Invoke the function "check_divisibility" using the following scenarios:
 # - 10, 2
 # - 7, 3
-print(check_divisibility(10, 2))
-print(check_divisibility(7, 3))
+print(check_divisibility(10, 2)) #True
+print(check_divisibility(7, 3)) #False
