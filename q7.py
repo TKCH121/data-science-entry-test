3bb64235dc3aa059530db27ae4c05563c599b683
TKCH121 <timothykoo@hotmail.com>
2026-06-23 14:00:31 +0800

    Update q7.py

--- q7.py
+++ q7.py
@@ -7,16 +7,16 @@
 
 class Car:
     def __init__(self, make, model, year):
-        self.make = make
+        self.make = make 
         self.model = model
-        self.year = year
+        self.year = year #creating three instances "make", "model" and "year" and stored as attributes inside a class
 
     def describe_car(self):
-        print(f"{self.year} {self.make} {self.model}")
+        print(f"{self.year} {self.make} {self.model}") #prints information about the car in the order presribed
 
 
 # Task 2
 # Create an instance of the Car class with the following attributes and call describe_car method:
 # - Make: Toyota, Model: Corolla, Year: 2020
 car1 = Car("Toyota", "Corolla", 2020)
-car1.describe_car()
+car1.describe_car() #2020 Toyota Corolla ("year", "make", "model")
